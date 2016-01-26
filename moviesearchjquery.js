7759abb0e570259c2951ea91cf4dd459a415973d
Allison <allison.donlevy@generalassemb.ly>
2016-01-25 18:01:11 -0800

    all kindsa

---
+++ moviesearchjquery.js
@@ -0,0 +1,35 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+
+
+$("#movie-search-form").on;
+("submit", formSubmitted);
+
+$("#query").value("http://omdbapi.com/?s=");
+$.get(url, resultsReceived);
+
+
+
+$("<li>")
+$("<a>").text(movieTitle)
+$("<div>")
+
+
+
+item.text(movie["Title"])
+item.text(movie["Year"];
+
+
+link.attr("href", url);
+
+
+
+item.addClass("movie");
+item.addClass("movie-title");
+item.addClass("movie-release-date");
+
+
+title.append(link);
+item.append(title);
+item.append(release);
+item.appendTo("#movies")
